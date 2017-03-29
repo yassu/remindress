0f70eca8ff07b612f35da39225df0a39ef5b58c2
yassu <mathyassu@gmail.com>
2017-03-30 03:25:58 +0900

    fix indentation of index.js

--- index.js
+++ index.js
@@ -6,17 +6,20 @@ const BrowserWindow = electron.BrowserWindow;
 let mainWindow;
 
 // 全てのウィンドウが閉じたら終了
-app.on('window-all-closed', function() {
-  if (process.platform != 'darwin') {
+app.on('window-all-closed', function()
+{
+  if (process.platform != 'darwin')
+  {
     app.quit();
   }
 });
 
 // Electronの初期化完了後に実行
-app.on('ready', function() {
+app.on('ready', function()
+{
   // メイン画面の表示。ウィンドウの幅、高さを指定できる
-    mainWindow = new BrowserWindow({width: 800, height: 600, minWidth: 800, maxWidth: 800,
-				    minHeight: 600, maxHeight: 600, center: true});
+  mainWindow = new BrowserWindow({width: 800, height: 600, minWidth: 800,
+    maxWidth: 800, minHeight: 600, maxHeight: 600, center: true});
   mainWindow.loadURL('file://' + __dirname + '/index.html');
 
   // ウィンドウが閉じられたらアプリも終了
